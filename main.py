422317caa5dd321849476092147db538ecbfc517
ThomasGGoodwin <thomas.gabriel.goodwin@gmail.com>
2025-08-31 09:17:12 -0500

    Adding basic form to lookup verse

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template('index.html', name = "Unknown User")
+
+@app.route("/lookup_verse", methods = ['POST', 'GET'])
+def lookup_verse():
+    if request.method == 'POST':
+        book = request.form['book']
+        chapter = request.form['chapter']
+        verse = request.form['verse']
+        return render_template('found_verse.html', book = book, chapter = chapter, verse = verse)
+    else:
+        return render_template('lookup_verse.html')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port = 8080, debug=True)
